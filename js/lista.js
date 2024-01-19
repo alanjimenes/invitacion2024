f6482b321e26c5c41103d470a0570bc77f41ebc3
Alan Jimenez <64500750+alanjimenes@users.noreply.github.com>
2024-01-19 10:58:05 -0400

    Sistema de Invitaciones 2024

---
+++ js/lista.js
@@ -0,0 +1,581 @@
+const lista_invitados_hombres = [
+  {
+    nombre: "Alan Jimenez",
+    invitados: [
+      "Laura Camila Jimenez (Alan Jimenez)",
+      "Daury Jimenez Cruz (Alan Jimenez)",
+      "Daury Jimenez Quezada (Alan Jimenez)",
+      "Laura Valeria Quezada (Alan Jimenez)",
+      "Alba Maria Garcia Quezada (Alan Jimenez)",
+      "Franyany Rodriguez (Alan Jimenez)",
+      "Jose Gabriel Guerra (Alan Jimenez)",
+      "Paul Guerra Rosado (Alan Jimenez)",
+      "Juan (Alan Jimenez)"
+    ]
+  },
+  {
+    nombre: "Ashley Restituyo",
+    invitados: [
+      "Franyeli Capellan (Ashley Restituyo)",
+      "Leah Restituyo (Ashley Restituyo)",
+      "Ashly Taveras (Ashley Restituyo)",
+      "Reyli David Cedaño Taveras (Ashley Restituyo)",
+      "Eddy Batista (Ashley Restituyo)",
+      "Cristopher Batista (Ashley Restituyo)",
+      "Josue Abreu (Ashley Restituyo)",
+      "Rachel Capellan Candelario (Ashley Restituyo)",
+      "Massiel Capellán Candelario (Ashley Restituyo)",
+      "Jonathan Baez (Ashley Restituyo)",
+      "Jose Alfredo Diaz Aquino (Ashley Restituyo)",
+      "Alis Cristal Santana (Ashley Restituyo)",
+      "Luis Marte (Ashley Restituyo)",
+      "Angela Batista (Ashley Restituyo)",
+      "Milka Batista (Ashley Restituyo)",
+      "Eliazar Quezada (Ashley Restituyo)",
+      "Alberto Vasquez (Ashley Restituyo)",
+      "Damaris Candelario (Ashley Restituyo)",
+      "Colon Capellan (Ashley Restituyo)" ]
+  },
+  {
+    nombre: "Angel Jose Queliz",
+    invitados: [
+      "Anselmo Queliz (Angel Jose Queliz)",
+      "Angela Vázquez (Angel Jose Queliz)",
+      "Amado (Angel Jose Queliz)",
+      "Miguel Ángel (Angel Jose Queliz)",
+      "Abuela (Angel Jose Queliz)",
+      "Abuelo (Angel Jose Queliz)",
+      "Tía Jaquelín (Angel Jose Queliz)",
+      "Briandra (Angel Jose Queliz)",
+      "María de los Ángeles (Angel Jose Queliz)",
+      "Brenda abad Abreu (Angel Jose Queliz)",
+      "Derlin (Angel Jose Queliz)",
+      "Erick maceta (Angel Jose Queliz)"
+    ]
+  },
+  {
+    nombre: "Helmer De Jesus GIl Hurtado",
+    invitados: [
+      "Clary Dania Hurtado (Helmer De Jesus GIl Hurtado)",
+      "Soraida Hurtado (Helmer De Jesus GIl Hurtado)",
+      "Andrés Hurtado (Helmer De Jesus GIl Hurtado)",
+      "Soranyi Rosado (Helmer De Jesus GIl Hurtado)",
+      "Bianka VIctoriano (Helmer De Jesus GIl Hurtado)",
+      "Lissette Duran (Helmer De Jesus GIl Hurtado)",
+      "Cesilia Batista (Helmer De Jesus GIl Hurtado)",
+      "Franchesca Victoriano (Helmer De Jesus GIl Hurtado)",
+      "Hansell GIl Cabral (Helmer De Jesus GIl Hurtado)"
+    ]
+  },
+    {
+      nombre: "Angel Francisco",
+      invitados: [
+        "Milagros María Delgado (Angel Francisco)",
+        "Camila Esther Rosa Delgado (Angel Francisco)",
+        "Adriana Lucia Rosa Delgado (Angel Francisco)"
+      ]
+    },
+    {
+      nombre: "Ana Quezada",
+      invitados: [
+        "Yissel Rosario Abreu (Ana Quezada)",
+        "Yilexis Quezada Rosario (Ana Quezada)",
+        "Angel Salvador Quezada (Ana Quezada)",
+        "Alexandra Quezada Frías (Ana Quezada)",
+        "Alejandra Quezada Batista (Ana Quezada)",
+        "Yanet Quezada (Ana Quezada)",
+        "Alexis Quezada (Ana Quezada)",
+        "Danerid Trinidad (Ana Quezada)",
+        "Josefina Cruz (Ana Quezada)",
+        "Mailin (Ana Quezada)",
+        "Arileisy Quezada (Ana Quezada)",
+        "Alexa Abreu (Ana Quezada)",
+        "Felo (Ana Quezada)"
+      ]
+    },
+    {
+      nombre: "Adelis Díaz",
+      invitados: [
+        "Manuel Alb. Díaz (Adelis Díaz)",
+        "María Díaz (Adelis Díaz)",
+        "Juan Pablo Ramírez (Adelis Díaz)",
+        "Yaciel Pichardo (Adelis Díaz)",
+        "Gerald Taveras (Adelis Díaz)",
+        "Emirson Díaz (Adelis Díaz)",
+        "Ronny Pichardo (Adelis Díaz)",
+        "Randy Pichardo (Adelis Díaz)",
+        "Arianny Padilla (Adelis Díaz)",
+        "Ana María Padilla (Adelis Díaz)",
+        "Elianny Jiménez (Adelis Díaz)",
+        "Susi Padilla (Adelis Díaz)",
+        "Alberto Rosa (Adelis Díaz)",
+        "Gabriel Jiménez (Adelis Díaz)",
+        "Isael Jimenez (Adelis Díaz)",
+        "Angelica Páez (Adelis Díaz)"
+      ]
+    },
+    {
+      nombre: "Valeria Piña",
+      invitados: [
+        "Ángela (Valeria Piña)",
+        "Karini (Valeria Piña)",
+        "Daniela (Valeria Piña)",
+        "Euri (Valeria Piña)",
+        "Grecia (Valeria Piña)",
+        "Almida (Valeria Piña)",
+        "Victoria (Valeria Piña)",
+        "Lía (Valeria Piña)",
+        "María José (Valeria Piña)",
+        "Amanda (Valeria Piña)"
+      ]
+    },
+    {
+      nombre: "Kendry Hernandez",
+      invitados: [
+        "Rafael Antonio (Kelly) (Kendry Hernandez)",
+        "Dhariana Abreu Cruz (Kendry Hernandez)",
+        "Lorenzo Adames (Yajari) (Kendry Hernandez)",
+        "Ramona Cruz (Kendry Hernandez)",
+        "Diana Deschamps (Kendry Hernandez)",
+        "Emeli Martes (Kendry Hernandez)",
+        "Erimar Brithar (Kendry Hernandez)",
+        "Kevin Arias (Kendry Hernandez)",
+        "Jose Angel (Kendry Hernandez)"
+      ]
+    },
+    {
+      nombre: "Lucia Piña",
+      invitados: [
+        "Berenice Ruiz (Lucia Piña)",
+        "Luis Piña (Lucia Piña)",
+        "Luis Piña (hijo) (Lucia Piña)",
+        "Rosa Galván (Lucia Piña)",
+        "Jonás Galván (Lucia Piña)",
+        "Rosanna Peralta (Lucia Piña)",
+        "Yohanny Parra (Lucia Piña)"
+      ]
+    },
+    {
+      nombre: "Kevin Suriel",
+      invitados: [
+        "Yeanne Marie Gutiérrez (Kevin Suriel)",
+        "Keiry Rosado (Kevin Suriel)",
+        "Daniela Abreu Báez (Kevin Suriel)",
+        "Martín Suriel (Kevin Suriel)",
+        "Martín Suriel (hijo) (Kevin Suriel)",
+        "Cristin Suriel (Kevin Suriel)",
+        "Yuberkys Báez (Kevin Suriel)",
+        "Aldrin Victoriano (Kevin Suriel)",
+        "José Rubén Rosa (Kevin Suriel)",
+        "Alfred Báez (Kevin Suriel)",
+        "Anderson Abreu (Kevin Suriel)"
+      ]
+    },
+    {
+      nombre: "Isabela Anazagatis",
+      invitados: [
+        "Eduardo Almonte (Isabela Anazagatis)",
+        "Frandy García (Isabela Anazagatis)",
+        "Lixander Castillo (Isabela Anazagatis)",
+        "Mónica Castillo (Isabela Anazagatis)",
+        "Carlos Castillo (Isabela Anazagatis)",
+        "Miguel Ángel Lebrón (Isabela Anazagatis)"
+      ]
+    },
+    {
+      nombre: "Adry Michell Abreu Bueno",
+      invitados: [
+        "Johuel Abreu Gratereaux (Adry Michell Abreu Bueno)",
+        "Adiza Díaz (Adry Michell Abreu Bueno)",
+        "Fátima García (Adry Michell Abreu Bueno)",
+        "José Frank Candelario (Adry Michell Abreu Bueno)",
+        "Sophy Candelario (Adry Michell Abreu Bueno)",
+        "Ashley Herrera (Adry Michell Abreu Bueno)",
+        "Adrian Miguel Candelario (Adry Michell Abreu Bueno)",
+        "Kendry Báez (Adry Michell Abreu Bueno)",
+        "Gabriela de los Santos Bueno (Adry Michell Abreu Bueno)",
+        "Willianny de los Santos Bueno (Adry Michell Abreu Bueno)",
+        "Susanny Candelario (Adry Michell Abreu Bueno)",
+        "Marien Bueno (Adry Michell Abreu Bueno)",
+        "Yiselina Bueno (Adry Michell Abreu Bueno)",
+        "Elizabeth Abreu Gratereaux (Adry Michell Abreu Bueno)",
+        "Nieves Suriel Pérez (Adry Michell Abreu Bueno)"
+      ]
+    },
+    {
+      nombre: "Luz Neyri",
+      invitados: [
+        "Luz Mercedes Rosa (Luz Neyri)",
+        "Scarlett Delgado (Luz Neyri)",
+        "Mercedes Santos (Luz Neyri)",
+        "Iranna Yissel Jiménez (Luz Neyri)",
+        "Elizabeth Rosa (Luz Neyri)",
+        "Amanda (Luz Neyri)",
+        "Germayoni (Luz Neyri)",
+        "María Elizardi (Luz Neyri)",
+        "Emely Lagares (Luz Neyri)",
+        "Edyleiny Victoriano (Luz Neyri)",
+        "Daniela Diez (Luz Neyri)",
+        "Marcos Abud (Luz Neyri)",
+        "Jhonse Espinosa (Luz Neyri)",
+        "Diógenes Batista (Luz Neyri)",
+        "Ester Elizabet (Luz Neyri)",
+        "Dariel Duran (Luz Neyri)",
+        "Estarling 'Tao' (Luz Neyri)"
+      ]
+    },  
+    {
+      nombre: "Leanny Maria Duran",
+      invitados: [
+        "Angelica Duran (Leanny Maria Duran)",
+        "Nidio Duran (Leanny Maria Duran)",
+        "Lorena Lorenzo (Leanny Maria Duran)",
+        "Angel Duran (Leanny Maria Duran)",
+        "Elianny Ramírez (Leanny Maria Duran)",
+        "Johanny Ramírez (Leanny Maria Duran)",
+        "Ángela Santos (Leanny Maria Duran)",
+        "Eusebia Marte (Leanny Maria Duran)",
+        "Yajaira González (Leanny Maria Duran)",
+        "Danelson Rodríguez (Leanny Maria Duran)",
+        "Frendy Serranta Marte (Leanny Maria Duran)",
+        "Maria Milagros de los Santos (Leanny Maria Duran)",
+        "Rosa de Jesús (Leanny Maria Duran)"
+      ]
+    },
+    {
+      nombre: "Alfonso Cabral",
+      invitados: [
+        "María Cabral (Alfonso Cabral)",
+        "Yaquelin Cabral (Alfonso Cabral)",
+        "Skarlett Cabral (Alfonso Cabral)",
+        "Axel Peralta (Alfonso Cabral)",
+        "Saúl Cruz (Alfonso Cabral)",
+        "José Ángel Quéliz (Alfonso Cabral)",
+        "Marcos Abreu (Alfonso Cabral)",
+        "Jose Juan Leira (Alfonso Cabral)",
+      "Luis Alberto Collado (Alfonso Cabral)"
+      ]
+    },
+    {
+      nombre: "Angel Nathanael Castillo",
+      invitados: [
+        "María del Carmen Pérez Plasencia (Angel Nathanael Castillo)",
+        "Rafael Antonio Castillo Quezada (Angel Nathanael Castillo)",
+        "Irángel de Jesús Castillo Pérez (Angel Nathanael Castillo)",
+        "Moisés Elián Castillo (Angel Nathanael Castillo)",
+        "Gladys Espinal (Angel Nathanael Castillo)",
+        "Branlly Pérez (Angel Nathanael Castillo)",
+        "Aymme Acevedo (Angel Nathanael Castillo)",
+        "Braudhy de los Santos (Angel Nathanael Castillo)",
+        "Franchesca Suriel (Angel Nathanael Castillo)",
+        "Alondra Acevedo (Angel Nathanael Castillo)",
+        "Oranyi Diaz (Angel Nathanael Castillo)",
+        "Raynell Cruz (Angel Nathanael Castillo)",
+        "Valery Nuñez (Angel Nathanael Castillo)"
+      ]
+    },
+    {
+      nombre: "Yaccelys Rodriguez",
+      invitados: [
+        "Franchesca (Yaccelys Rodriguez)",
+        "Ramón (Yaccelys Rodriguez)",
+        "Alvin (Yaccelys Rodriguez)",
+        "Francis (Yaccelys Rodriguez)",
+        "Jaquelín (Yaccelys Rodriguez)",
+        "Dilany (Yaccelys Rodriguez)",
+        "Sebastian Feliz (Yaccelys Rodriguez)",
+        "Albert (Yaccelys Rodriguez)",
+        "Malvin (Yaccelys Rodriguez)",
+        "Ángel (Yaccelys Rodriguez)",
+        "Braylin (Yaccelys Rodriguez)",
+        "Alberto (Yaccelys Rodriguez)",
+        "Raudy (Yaccelys Rodriguez)"
+      ]
+    },
+    {
+      nombre: "Diego Felipe Ferrer",
+      invitados: [
+        "Salvador (Diego Felipe Ferrer)",
+        "Dayhana (Diego Felipe Ferrer)",
+        "Vienchy (Diego Felipe Ferrer)",
+        "Manolo (Diego Felipe Ferrer)",
+        "Josefina (Diego Felipe Ferrer)",
+        "Percio (Diego Felipe Ferrer)",
+        "Maritza (Diego Felipe Ferrer)",
+        "Ana milagros (Diego Felipe Ferrer)",
+        "Sahiana (Diego Felipe Ferrer)",
+        "Monse (Diego Felipe Ferrer)",
+        "Anirka (Diego Felipe Ferrer)"
+      ]
+    },
+    {
+      nombre: "Leslie Peguero",
+      invitados: [
+        "Argentina Gratero (Leslie Peguero)",
+        "Grecia Gonzalez (Leslie Peguero)",
+        "Karla Siri (Leslie Peguero)"
+      ]
+    },
+    {
+      nombre: "Fraylin",
+      invitados: [
+        "Leticia Hernández (Fraylin)",
+        "Kevin Hernández (Fraylin)",
+        "María (Fraylin)",
+        "Leydi (Fraylin)",
+        "Winar (Fraylin)",
+        "Elien (Fraylin)",
+        "Alexis (Fraylin)",
+        "Kendry Duran (Fraylin)",
+        "Yaneli Batista (Fraylin)"
+      ]
+    },
+    {
+    nombre: "Karen Cuevas",
+    invitados: [
+      "José Cuevas (Karen Cuevas)",
+      "José Julio Cuevas (Karen Cuevas)",
+      "Juana Mora (Karen Cuevas)",
+      "Katherine Cuevas (Karen Cuevas)",
+      "Lismerlin Jiménez (Karen Cuevas)",
+      "Jian Suriel (Karen Cuevas)",
+      "Alexandro Victoriano (Karen Cuevas)",
+      "Manuel Mora (Karen Cuevas)",
+      "Rossiel Batista (Karen Cuevas)"
+    ]
+  },  
+  { 
+  nombre: "Eddy Jimenez",
+  invitados: [
+    "Odrix (Eddy Jimenez)",
+    "Arlenis (Eddy Jimenez)",
+    "Arlett (Eddy Jimenez)",
+    "Gisauri (Eddy Jimenez)",
+    "Candy (Eddy Jimenez)",
+    "Mariela (Eddy Jimenez)",
+    "Ranses (Eddy Jimenez)",
+    "Keyla (Eddy Jimenez)",
+    "Makiely (Eddy Jimenez)",
+    "Thulsy (Eddy Jimenez)",
+    "Maiky (Eddy Jimenez)"
+  ]   
+    
+} ,
+{
+nombre: "Carolin Jiménez",
+invitados: [
+  "Aridis Jiménez (Carolin Jiménez)",
+  "Karina Gutiérrez (Carolin Jiménez)",
+  "Maria Ferreira (Carolin Jiménez)",
+  "Leomar Gutiérrez (Carolin Jiménez)",
+  "Fernanda Gutiérrez (Carolin Jiménez)",
+  "Raymond (Carolin Jiménez)",
+  "Richard (Carolin Jiménez)"
+]
+},
+{
+  nombre: "Enrique Rosa Veloz",
+  invitados: [
+    "Jennifer Veloz (Enrique Rosa Veloz)",
+    "Alexandra Veloz (Enrique Rosa Veloz)",
+    "Coraima Veloz (Enrique Rosa Veloz)",
+    "Rouss de los santos (Enrique Rosa Veloz)",
+    "Matilda Veloz (Enrique Rosa Veloz)",
+    "Jean Carlos Durán (Enrique Rosa Veloz)",
+    "Anicelis veloz (Enrique Rosa Veloz)",
+    "Mercedes veloz (Enrique Rosa Veloz)",
+    "Andrea (Enrique Rosa Veloz)",
+    "Carmelia Moreno (Enrique Rosa Veloz)",
+    "Angelica Guzman (Enrique Rosa Veloz)",
+    "Isaylen (Enrique Rosa Veloz)",
+    "Jhon Payano (Enrique Rosa Veloz)"
+  ]
+},
+{
+  nombre: "Erick Enger",
+  invitados: [
+    "Erianna Massiel Candelario (Erick Enger)",
+    "Andriel Santana (Erick Enger)",
+    "Jenny Candelario (Erick Enger)",
+    "Eriangeles Candelario (Erick Enger)",
+    "Adaya Marte (Erick Enger)",
+    "Alvaro Candelario (Erick Enger)",
+    "Andy Candelario (Erick Enger)",
+    "Edwind Manuel (Erick Enger)",
+    "Oliver Pichardo (Erick Enger)",
+    "Iván Paulino (Erick Enger)",
+    "Braylin Abud (Erick Enger)",
+    "Danny Junior (Erick Enger)",
+    "Massiel Rodríguez (Erick Enger)",
+    "Keimi Fernández (Erick Enger)",
+    "Lisaury Bueno (Erick Enger)",
+    "Josue Domínguez (Erick Enger)",
+    "Roció Candelario (Erick Enger)"
+  ]
+},
+{
+  nombre: "Lianny Isabel",
+  invitados: [
+    "Karla Marina (Lianny Isabel)",
+    "Jennifer Leonardo (Lianny Isabel)",
+    "Lisaudry Gil (Lianny Isabel)",
+    "Rous Jiménez (Lianny Isabel)",
+    "Arianny Abreu (Lianny Isabel)",
+    "Juana Calderón (Lianny Isabel)",
+    "Ángel Miguel (Lianny Isabel)",
+    "Julia Espinosa (Lianny Isabel)"
+  ]
+},
+{
+  nombre: "Cesar Emilio Marte",
+  invitados: [
+    "Kathy Quezada (Cesar Emilio Marte)",
+    "Miguel Angel Nuñez (Cesar Emilio Marte)",
+    "Shairy Baez (Cesar Emilio Marte)",
+    "Jeanne Marie Gutierrez (Cesar Emilio Marte)",
+    "Lorena Marte (Cesar Emilio Marte)",
+    "Franseli Tavarez (Cesar Emilio Marte)",
+    "Daholy Gutierrez (Cesar Emilio Marte)",
+    "Daury Hernández (Cesar Emilio Marte)",
+    "Innysh abud (Cesar Emilio Marte)",
+    "Steven Jiménez (Cesar Emilio Marte)",
+    "Darwin Baez (Cesar Emilio Marte)"
+  ]
+},
+{
+  nombre: "Cesar Mariñez",
+  invitados: [
+    "Amilka Perez (Cesar Mariñez)",
+    "Martha Bueno (Cesar Mariñez)",
+    "Cesarina Bueno (Cesar Mariñez)",
+    "Julio Cesar Pérez (Cesar Mariñez)",
+    "Eduardo Mariñez (Cesar Mariñez)",
+    "Yamilet Perez (Cesar Mariñez)",
+    "Elian Rey (Cesar Mariñez)",
+    "Clarisleidy Ferrer (Cesar Mariñez)",
+    "Bretón (Cesar Mariñez)",
+    "Nidia Victoriano (Cesar Mariñez)",
+    "Agustín Pérez (Cesar Mariñez)"
+  ]
+},
+{
+  nombre: "Starling García",
+  invitados: [
+    "Alin Quiroz (Starling García)",
+    "Skarly García (Starling García)",
+    "Rudy Manuel (Starling García)",
+    "Ruth García (Starling García)",
+    "Frandy Suriel (Starling García)",
+    "Sebastián García (Starling García)",
+    "Darwin José (Starling García)"
+  ]
+},
+{
+  nombre: "Verlin Perez",
+  invitados: [
+    "Veronica Ciprian (Verlin Perez)",
+    "Hector Perez (Verlin Perez)",
+    "Manuel Estevez (Verlin Perez)",
+    "Cristopher Pérez (Verlin Perez)",
+    "Lismairy Ciprian (Verlin Perez)",
+    "Sáhara Pérez (Verlin Perez)",
+    "José Adrián Ciprian (Verlin Perez)",
+    "Ashley Rodríguez (Verlin Perez)"
+  ]
+}
+    
+
+  ];
+
+
+  
+let colaboradores = [
+  "Luis Alfredo",
+  "José Andrés",
+  "Dahian",
+  "Brandol",
+  "Robinson(Dance King)"
+     ];
+
+     let alegria = [
+      "Ángel Sánchez (Alegria)",
+      "Marelin (Alegria)",
+      "Elidanny (Alegria)",
+      "Yumi (Alegria)",
+      "Adrián (Alegria)",
+      "Ana Belén (Alegria)",
+      "Elian (Alegria)",
+      "Leandro (Alegria)",
+      "Melfrit (Alegria)",
+      "Valentina (Alegria)",
+      "Penelope (Alegria)",
+      "Keury (Alegria)",
+      "Marelin (Alegria)",
+      "Yarrisfel (Alegria)",
+      "Kimberlin (Alegria)",
+      "Waldy (Alegria)",
+      "Arlin (Alegria)",
+      "Carlos (Alegria)",
+      "Juanki (Alegria)",
+      "Edward (Alegria)",
+      "Edeyerly (Alegria)",
+      "Angel Marte (Alegria)"
+  ];
+
+
+//Logica
+function insertar_lista() {
+  lista_invitados_hombres.forEach((invitado) => {
+      invitado.invitados.forEach((nombre) => {
+          document.querySelector(".lista").innerHTML += `<p>${nombre}</p>`;
+      });
+  });
+
+  colaboradores.forEach((nombre) => {
+      document.querySelector(".lista").innerHTML += `<p>${nombre}</p>`;
+  });
+
+  alegria.forEach((nombre) => {
+      document.querySelector(".lista").innerHTML += `<p>${nombre}</p>`;
+  });
+}
+
+insertar_lista();
+
+let busqueda = document.querySelector(".busqueda");
+
+busqueda.addEventListener("input", function() {
+    let html = "";
+    const valorBuscado = busqueda.value.toLowerCase(); // convertir valor buscado a minúsculas
+
+    for (let i = 0; i < lista_invitados_hombres.length; i++) {
+
+      for (let x = 0; x < lista_invitados_hombres[i].invitados.length; x++) {
+
+        const invitado = lista_invitados_hombres[i].invitados[x].toLowerCase(); // convertir elemento de array a minúsculas
+        
+        if (invitado.includes(valorBuscado)) {
+          html += `<p>${lista_invitados_hombres[i].invitados[x]}</p>`;
+        }
+
+      }
+    }
+
+    for (let a = 0; a < colaboradores.length; a++){
+      const colaborador = colaboradores[a].toLowerCase(); // convertir elemento de array a minúsculas
+      if (colaborador.includes(valorBuscado)){
+       html += `<p>${colaboradores[a]}</p>`;
+      }
+    }
+
+    for (let a = 0; a < alegria.length; a++){
+
+      const alegriaItem = alegria[a].toLowerCase(); // convertir elemento de array a minúsculas
+      if (alegriaItem.includes(valorBuscado)){
+       html += `<p>${alegria[a]}</p>`;
+      }
+
+    }
+
+    document.querySelector(".lista").innerHTML = html;
+  });
